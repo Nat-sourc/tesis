b2e1d29889d57a129eea085c7865f1388769e3fc
Lau2906 <La_jimenez@javeriana.edu.co>
2023-06-04 19:58:49 -0500

    explicacion tarea 1

--- lib/src/pages/bradicinesia.dart
+++ lib/src/pages/bradicinesia.dart
@@ -1,3 +1,4 @@
+import 'package:brainFit/src/components/titleImg.dart';
 import 'package:flutter/material.dart';
 
 class Bradicinesia extends StatelessWidget {
@@ -5,6 +6,100 @@ class Bradicinesia extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+        backgroundColor: Colors.white,
+        body: ListView(
+          children: [
+            Container(
+                padding: const EdgeInsets.all(10),
+                child: Column(children: [
+                  const Row(
+                    children: [
+                      SizedBox(
+                        width: 30.0,
+                        height: 100.0,
+                      )
+                    ],
+                  ),
+                  const TitleImg(),
+                  const Row(
+                    children: [
+                      SizedBox(
+                        width: 30.0,
+                        height: 80.0,
+                      )
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset(
+                        "assets/img/bradicinesia.png",
+                      ),
+                    ],
+                  ),
+                  const Row(
+                    children: [
+                      SizedBox(
+                        width: 30.0,
+                        height: 80.0,
+                      )
+                    ],
+                  ),
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("Bradicinesis",
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontFamily: 'RobotoMono-Bold',
+                              fontSize: 30))
+                    ],
+                  ),
+                  const Row(
+                    children: [
+                      SizedBox(
+                        width: 30.0,
+                        height: 50.0,
+                      )
+                    ],
+                  ),
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Flexible(
+                        child: Text(
+                            "Esta a punto de empezar el procedimiento de Bradicinesis",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontFamily: 'RobotoMono-Bold',
+                                fontSize: 18)),
+                      )
+                    ],
+                  ),
+                  const Row(
+                    children: [
+                      SizedBox(
+                        width: 30.0,
+                        height: 50.0,
+                      )
+                    ],
+                  ),
+                  ElevatedButton(
+                    onPressed: () => {Navigator.of(context).pushNamed("/taskFist")},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color.fromARGB(255, 0, 191, 166),
+                      minimumSize: const Size(350, 50),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0)),
+                    ),
+                    child: const Text("Comencemos",
+                        style: TextStyle(
+                            fontFamily: 'RobotoMono-Bold', fontSize: 20)),
+                  ),
+                ])),
+          ],
+        ));
   }
 }
--- lib/src/pages/home.dart
+++ lib/src/pages/home.dart
@@ -96,7 +96,7 @@ class _HomeState extends State<Home> {
                     ],
                   ),
                   ElevatedButton(
-                    onPressed: () => {showPantallaBradicinesia("/taskFist")},
+                    onPressed: () => {showPantallaBradicinesia("/bradicinesia")},
                     style: ElevatedButton.styleFrom(
                       backgroundColor: const Color.fromARGB(255, 0, 191, 166),
                       minimumSize: const Size(350, 50),
