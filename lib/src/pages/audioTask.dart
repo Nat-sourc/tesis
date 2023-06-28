85b6b367addad8c8217244214e816c9395ca2a1a
Lau2906 <La_jimenez@javeriana.edu.co>
2023-06-27 21:41:49 -0500

    Arreglo navegabilidad

--- lib/src/pages/audioTask.dart
+++ lib/src/pages/audioTask.dart
@@ -1,4 +1,12 @@
 
+import 'package:brainFit/src/pages/button_Task.dart';
+import 'package:brainFit/src/pages/cameraDualTask.dart';
+import 'package:brainFit/src/pages/cameraMarcha.dart';
+import 'package:brainFit/src/pages/taskDual.dart';
+import 'package:brainFit/src/pages/taskMarcha.dart';
+import 'package:brainFit/src/pages/uploadDual.dart';
+import 'package:brainFit/src/pages/videoDualTask.dart';
+import 'package:brainFit/src/pages/videoMarcha.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:brainFit/src/providers/play_audio_provider.dart';
@@ -21,6 +29,14 @@ class AudioTask extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         title: 'Grabadora de voz',
         home: RecordAndPlayScreen(parameterValue: parameterValue),
+        routes: {"/buttonsTasks": (context) => const ButtonTaskDualTask(),
+        "/taskMarcha": (context) => const TaskMarcha(),
+        "/cameraMarcha": (context) => const CameraMarcha(),
+        "/videoMarcha": (context) => const VideoMarcha(),
+        "/taskDual": (context) => const TaskDual(),
+        "/cameraDual": (context) => const CameraDualTask(),
+        "/videoDual": (context) => const VideoDualTask(),
+        "/uploadDual": (context) => const UploadVideoDual(),},
       ),
     );
   }
